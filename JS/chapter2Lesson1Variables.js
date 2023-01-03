7bea348da21629faac9414f85d2c85c598d26031
OlegR <olegromanchuk777@gmail.com>
2023-01-03 16:07:31 +0300

    First Commit

---
+++ JS/chapter2Lesson1Variables.js
@@ -0,0 +1,55 @@
+
+// При названии переменных надо учитывать, что JavaScript является регистрозависимым языком, то есть в следующем коде объявлены две разные переменные:
+var username;
+var userName;
+
+// После определения переменной ей можно присвоить какое-либо значение. 
+// Для этого применяется оператор присваивания (=):
+// Процесс присвоения переменной начального значения называется инициализацией.
+var username = "";
+let userage = 37;
+
+//С помощью ключевого слова const можно определить константу, которая, как и переменная, хранит значение, однако это значение не может быть изменено.
+const username = "Tom";
+
+
+/**
+ * await, 
+ * break, 
+ * case, 
+ * catch, 
+ * class, 
+ * const, 
+ * continue, 
+ * debugger, 
+ * default, 
+ * delete, 
+ * do, 
+ * else, 
+ * enum, 
+ * export, 
+ * extends, 
+ * false, 
+ * finally, 
+ * for, 
+ * function, 
+ * if, 
+ * import, 
+ * in, 
+ * instanceof, 
+ * new, 
+ * null, 
+ * return, 
+ * super, 
+ * switch, 
+ * this, 
+ * throw, 
+ * true, 
+ * try, 
+ * typeof, 
+ * var, 
+ * void, 
+ * while, 
+ * with, 
+ * yield
+ */
